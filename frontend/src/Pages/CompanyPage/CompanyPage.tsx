bb7e4e67b5d1704492d03fefa37c70dbfb0510e9
marcohool <marco.boutchelski@gmail.com>
2024-04-11 18:35:52 +0100

    Initialise Company page & UseEffect

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
 
 interface Props {}
 
 export const CompanyPage: React.FC<Props> = (props) => {
-  return <></>;
+  let { ticker } = useParams<{ ticker: string }>();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+
+      if (!result.error) {
+        setCompany(result.data[0]);
+      } else {
+        console.error(result.error);
+      }
+    };
+
+    getProfileInit();
+  }, []);
+
+  return (
+    <>{company ? <div>{company.companyName}</div> : <div>Loading...</div>}</>
+  );
 };
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 export interface SearchResponse {
   data: CompanySearch[];
+}
+
+interface ApiResponse<T> {
+  data: T;
+  error?: string;
 }
 
 export const searchCompanies = async (query: string) => {
@@ -17,6 +22,24 @@ export const searchCompanies = async (query: string) => {
     } else {
       console.log("unexpected error: ", error);
       return "An expected error has occured.";
+    }
+  }
+};
+
+export const getCompanyProfile = async (
+  query: string,
+): Promise<ApiResponse<CompanyProfile[]>> => {
+  try {
+    return await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_FMP_API_KEY}`,
+    );
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return { data: [], error: error.message };
+    } else {
+      console.log("unexpected error: ", error);
+      return { data: [], error: "An unexpected error has occurred." };
     }
   }
 };
