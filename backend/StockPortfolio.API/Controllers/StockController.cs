20142ed0f96c1773e6e812280bd5e850c542bc07
marcohool <marco.boutchelski@gmail.com>
2024-04-20 02:11:52 +0100

    Stock Controller added

---
+++ backend/StockPortfolio.API/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockPortfolio.API.Data;
+
+namespace StockPortfolio.API.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Stocks.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
