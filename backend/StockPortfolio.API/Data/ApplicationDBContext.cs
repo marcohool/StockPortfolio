bf811ae5e2288d18c28f9e85883ab7cc4b52d5a8
marcohool <marco.boutchelski@gmail.com>
2024-04-20 01:58:33 +0100

    Entity Framework added

---
+++ backend/StockPortfolio.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockPortfolio.API.Models;
+
+namespace StockPortfolio.API.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOptions)
+            : base(dbContextOptions) { }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
