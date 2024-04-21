818633ef65752d1f33b7df96237a30a7724e70e9
marcohool <marco.boutchelski@gmail.com>
2024-04-21 14:04:39 +0100

    Stock DELETE

--- backend/StockPortfolio.API/Controllers/StockController.cs
+++ backend/StockPortfolio.API/Controllers/StockController.cs
@@ -72,5 +72,22 @@ namespace StockPortfolio.API.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
