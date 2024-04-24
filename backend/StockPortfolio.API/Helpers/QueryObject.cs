b8b41e779f939c614c0c28c48872272e8e754f00
marcohool <marco.boutchelski@gmail.com>
2024-04-24 18:32:49 +0100

    Paginiation data annotation validation

--- backend/StockPortfolio.API/Helpers/QueryObject.cs
+++ backend/StockPortfolio.API/Helpers/QueryObject.cs
@@ -1,4 +1,6 @@
-﻿namespace StockPortfolio.API.Helpers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockPortfolio.API.Helpers
 {
     public class QueryObject
     {
@@ -6,7 +8,11 @@
         public string? CompanyName { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool IsDecsending { get; set; } = false;
+
+        [Range(1, 1000)]
         public int PageNumber { get; set; } = 1;
+
+        [Range(1, 50)]
         public int PageSize { get; set; } = 20;
     }
 }
