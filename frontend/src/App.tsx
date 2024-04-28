3926d40849b97719d1ead1e164dc5f7f5d7daa6b
marcohool <marco.boutchelski@gmail.com>
2024-04-28 13:46:26 +0100

    Context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import "./react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Outlet />
-      <ToastContainer />
+      <UserProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
     </div>
   );
 }
---
+++ frontend/src/Context/useAuth.tsx
@@ -0,0 +1,118 @@
+import { UserProfile } from "../Models/User";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginAPI, registerAPI } from "../Services/AuthService";
+import { toast } from "react-toastify";
+import axios from "axios";
+
+type UserContextType = {
+  user: UserProfile | null;
+  token: string | null;
+  registerUser: (email: string, username: string, password: string) => void;
+  loginUser: (username: string, password: string) => void;
+  logoutUser: () => void;
+  isLoggedIn: () => boolean;
+};
+
+type Props = { children: React.ReactNode };
+
+const UserContext = React.createContext<UserContextType>({} as UserContextType);
+
+export const UserProvider = ({ children }: Props) => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState<string | null>(null);
+  const [user, setUser] = useState<UserProfile | null>(null);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    if (user && token) {
+      setUser(JSON.parse(user));
+      setToken(token);
+      axios.defaults.headers.common["Authosization"] = `Bearer ${token}`;
+    }
+    setIsReady(true);
+  }, []);
+
+  const registerUser = async (
+    email: string,
+    username: string,
+    password: string,
+  ) => {
+    await registerAPI(email, username, password)
+      .then((response) => {
+        if (response) {
+          localStorage.setItem("token", response.data.token);
+
+          const userObj = {
+            userName: response.data.userName,
+            email: response.data.email,
+          };
+
+          localStorage.setItem("user", JSON.stringify(userObj));
+
+          setToken(response?.data.token!);
+          setUser(userObj);
+
+          toast.success("User registered successfully");
+
+          navigate("/search");
+        }
+      })
+      .catch((error) => toast.warning("Server error occurred " + error));
+  };
+
+  const loginUser = async (username: string, password: string) => {
+    await loginAPI(username, password)
+      .then((response) => {
+        if (response) {
+          localStorage.setItem("token", response.data.token);
+
+          const userObj = {
+            userName: response.data.userName,
+            email: response.data.email,
+          };
+
+          localStorage.setItem("user", JSON.stringify(userObj));
+
+          setToken(response?.data.token!);
+          setUser(userObj);
+
+          toast.success("Login success");
+
+          navigate("/search");
+        }
+      })
+      .catch((error) => toast.warning("Server error occurred " + error));
+  };
+
+  const isLoggedIn = () => {
+    return !!user;
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setUser(null);
+    setToken(null);
+    navigate("/");
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        token,
+        registerUser,
+        loginUser,
+        logoutUser,
+        isLoggedIn,
+      }}
+    >
+      {isReady && children}
+    </UserContext.Provider>
+  );
+};
+
+export const useAuth = () => React.useContext(UserContext);
